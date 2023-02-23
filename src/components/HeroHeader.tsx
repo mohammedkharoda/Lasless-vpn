88b970a838e64d832a3dbc90117bbd2f3624cbfd
Mohammed kharoda <kharodawalam@gmail.com>
2023-02-23 08:56:12 +0530

    inital

---
+++ src/components/HeroHeader.tsx
@@ -0,0 +1,11 @@
+const HeroHeader = () => {
+  return (
+    <>
+      <div className="text-[35px] text-red-900 font-rubik">
+        This is Hero Section
+      </div>
+    </>
+  );
+};
+
+export default HeroHeader;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/forms")],
+};
