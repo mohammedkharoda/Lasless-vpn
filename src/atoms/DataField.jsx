3aafe24a73da7e5c1f46135aeef13f26211d93ee
MOHAMMED KHARODAWALA <49630634+mohammedkharoda@users.noreply.github.com>
2023-03-10 14:01:15 +0000

    making the map loop

--- src/atoms/DataField.jsx
+++ src/atoms/DataField.jsx
@@ -3,36 +3,42 @@ import { BsFillPersonFill } from "react-icons/bs";
 import { AiOutlineLine } from "react-icons/ai";
 import { FaServer } from "react-icons/fa";
 const DataField = () => {
+  const infoGraphics = [
+    {
+      icon: <AiOutlineLine />,
+      text: "90+",
+      desc: "Users",
+    },
+    {
+      icon: <FaServer />,
+      text: "50+",
+      desc: "Server",
+    },
+  ];
   return (
     <div>
       <div className="bg-white drop-shadow-lg w-full  py-[37px] px-[90px] mt-10">
-        {/* users */}
-        <div className="flex gap-8">
-          <div className="bg-orange-light-lz w-fit p-[16px] rounded-full">
-            <BsFillPersonFill className="text-orange-lz" size={20} />
-          </div>
-          <div>
-            <div className="text-[25px] text-dark-blue-lz leading-7 font-bold">
-              90+
-            </div>
-            <div className="font-normal text-[20px] leading-7">Users</div>
-          </div>
-          <div className="rotate-90">
-            <AiOutlineLine size={55} className="text-gray-lz" />
-          </div>
-          {/* server */}
-          <div className="bg-orange-light-lz w-fit p-[16px] rounded-full">
-            <FaServer className="text-orange-lz" size={20} />
-          </div>
-          <div>
-            <div className="text-[25px] text-dark-blue-lz leading-7 font-bold">
-              50+ 
-            </div>
-            <div className="font-normal text-[20px] leading-7">Server</div>
-          </div>
-          <div className="rotate-90">
-            <AiOutlineLine size={55} className="text-gray-lz" />
-          </div>
+        <div className="flex gap-4">
+          {infoGraphics.map((info) => {
+            return (
+              <>
+                <div className="bg-orange-light-lz w-fit p-[16px] rounded-full">
+                  <div className="text-orange-lz">{info.icon}</div>
+                </div>
+                <div>
+                  <div className="text-[25px] text-dark-blue-lz leading-7 font-bold">
+                    {info.text}
+                  </div>
+                  <div className="font-normal text-[20px] leading-7">
+                    {info.desc}
+                  </div>
+                </div>
+                <div className="rotate-90">
+                  <AiOutlineLine size={55} className="text-gray-lz" />
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </div>
