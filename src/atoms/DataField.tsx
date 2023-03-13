7b130e7511c5cae61c5f4f3f4c5596ce405133a5
mohammed kharoda <kharodawalam@gmail.com>
2023-03-13 22:21:07 +0530

    Added new Icon pack and render conditons fix

--- src/atoms/DataField.tsx
+++ src/atoms/DataField.tsx
@@ -1,25 +1,31 @@
 import React from "react";
-import { BsFillPersonFill } from "react-icons/bs";
+import { UilServer } from "@iconscout/react-unicons";
+import { UilUser } from "@iconscout/react-unicons";
+import { UilLocationPoint } from "@iconscout/react-unicons";
 import { AiOutlineLine } from "react-icons/ai";
-import { FaServer } from "react-icons/fa";
 const DataField = () => {
   const infoGraphics = [
     {
-      icon: <BsFillPersonFill />,
+      icon: <UilUser />,
       text: "90+",
       desc: "Users",
     },
     {
-      icon: <FaServer />,
+      icon: <UilServer />,
       text: "50+",
       desc: "Server",
+    },
+    {
+      icon: <UilLocationPoint />,
+      text: "30+",
+      desc: "Locations",
     },
   ];
   return (
     <div>
       <div className="bg-white drop-shadow-lg w-full  py-[37px] px-[90px] mt-10">
         <div className="flex gap-4">
-          {infoGraphics.map((info) => {
+          {infoGraphics.map((info, index) => {
             return (
               <>
                 <div className="bg-orange-light-lz w-fit flex items-center p-5 rounded-full">
@@ -33,9 +39,11 @@ const DataField = () => {
                     {info.desc}
                   </div>
                 </div>
-                <div className="rotate-90">
-                  <AiOutlineLine size={55} className="text-gray-lz" />
-                </div>
+                {index !== infoGraphics.length - 1 && (
+                  <div className="rotate-90">
+                    <AiOutlineLine size={55} className="text-gray-lz" />
+                  </div>
+                )}
               </>
             );
           })}
