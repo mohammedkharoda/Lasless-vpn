b8b6c9b777bc415ea17226e4ca6f6a5c85d77e6d
mohammed kharoda <kharodawalam@gmail.com>
2023-03-23 22:08:22 +0530

    added one side section of the footer and planned the other sides of footer and structure it in a particular manner

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date();
+  const presentYear = currentYear.getFullYear();
   return (
     <div>
       {/* outer div */}
       <div>
         {/* for the brand logo and other description */}
-        <div>
+        <div className="flex flex-col gap-4">
           {/* img*/}
-          <img src="/Images/Logo.svg" />
+          <img src="/Images/Logo.svg" alt="footer-logo" className="w-[149px]" />
+          <p className="font-rubik font-medium text-[16px] leading-[30px] text-gray-lz max-w-[340px] ">
+            LaslessVPN is a private virtual network that has unique features and
+            has high security.
+          </p>
+          {/*  icons*/}
+          <p className="font-rubik font-normal text-[16px] leading-[30px] text-gray-lz">
+            {presentYear}©LaslesVPN
+          </p>
         </div>
         {/* for the 3 section that is being after the logo and description */}
         <div></div>
